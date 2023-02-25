077962ea54adb2af8c696363109130ef2d98ae22
sajim-khan <shossain1427@gmail.com>
2023-02-25 20:09:52 +0600

    added call dynamic api and load dynamic data in display

--- js/countries.js
+++ js/countries.js
@@ -10,16 +10,34 @@ const displayCountry = countries => {
     //     console.log(info);
     // }
     countries.forEach(country => {
-        console.log(country.name.common);
+        console.log(country.cca2);
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country')
-        
         countryDiv.innerHTML = `
             <h4>Name: ${country.name.common} </h4>
             <h4>Capital: ${country.capital ? country.capital[0] : 'no capital'} </h4>
+            <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
         `
         countriesInfo.appendChild(countryDiv);
-    })
-    
+    })    
 }
+
+const loadCountryDetails = code => {
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    //console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showCountryDetails(data[0]))
+}
+
+const showCountryDetails = details => {
+    const countryDetails = document.getElementById('country-details')
+    countryDetails.innerHTML = `
+    <h2>Country Detail</h2>
+    <h3>Name: ${details.name.common}</h3>
+    <p>Capital: ${details.capital}</p>
+    <img src="${details.flags.png}" alt="">
+    `
+}
+
 loadCountries()
